79b8de971bda553ffea1582db7d18fe53784d81a
El Harrak <elharrakmohamed@hotmail.fr>
2022-05-29 22:58:37 +0200

    2 commit

--- src/main/java/com/example/webservicerestbetriebsstellealsjson/web/BetriebsstelleRestController.java
+++ src/main/java/com/example/webservicerestbetriebsstellealsjson/web/BetriebsstelleRestController.java
@@ -14,14 +14,14 @@ public class BetriebsstelleRestController {
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     @ResponseBody
     public ArrayList<Map> betriebsstellenList() throws Exception {
-        return   ReadCSV.readAll("C:\\Users\\Mohamedhrrk\\Downloads\\DBNetz-Betriebsstellenverzeichnis-Stand2018-04.csv");
+        return   ReadCSV.readAll("src/main/resources/DBNetz-Betriebsstellenverzeichnis-Stand2018-04.csv");
     }
     @GetMapping (path = "/{abk}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map betriebsstelleAbk(@PathVariable("abk") String abk) throws Exception {
-        return ReadCSV.readElement("C:\\Users\\Mohamedhrrk\\Downloads\\DBNetz-Betriebsstellenverzeichnis-Stand2018-04.csv",abk);
+        return ReadCSV.readElement("src/main/resources/DBNetz-Betriebsstellenverzeichnis-Stand2018-04.csv",abk);
     }
     @GetMapping (path = "/content/{teilAbk}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ArrayList<Map> betriebsstelleContTeilAbk(@PathVariable("teilAbk") String teilAbk) throws Exception {
-        return ReadCSV.readElementContTeilAbk("C:\\Users\\Mohamedhrrk\\Downloads\\DBNetz-Betriebsstellenverzeichnis-Stand2018-04.csv",teilAbk);
+        return ReadCSV.readElementContTeilAbk("src/main/resources/DBNetz-Betriebsstellenverzeichnis-Stand2018-04.csv",teilAbk);
     }
 }
